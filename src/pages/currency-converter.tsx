281d8e65d76994a98d39e57caeb7d99b81576044
Lacy Morrow <me@lacymorrow.com>
2023-03-24 11:24:31 -0400

    feat(currency-con): Adding currency converter page

---
+++ src/pages/currency-converter.tsx
@@ -0,0 +1,11 @@
+import CurrencyConverter from "../components/CurrencyConverter";
+
+const CurrencyConverterPage = () => {
+  return (
+    <div>
+      <CurrencyConverter />
+    </div>
+  );
+};
+
+export default CurrencyConverterPage;
